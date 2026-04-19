b80b952f3bd94379c0fd5a98918dea0231cfe345
PorfirioTramontana <porfirio.tramontana@gmail.com>
2026-04-19 16:02:47 +0200

    Pulizia classi

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -5,15 +5,10 @@ import java.util.ArrayList;
 public class Utente {
     private String login;
     private String password;
-    private ArrayList<Bacheca> bacheca;
 
     public Utente(String login, String password) {
         this.login = login;
         this.password = password;
-        bacheca = new ArrayList<>();
-        bacheca.add(new Bacheca("Universita",""));
-        bacheca.add(new Bacheca("Tempo LIbero",""));
-        bacheca.add(new Bacheca("Lavoro",""));
     }
 
     public boolean login(String login, String password) {
